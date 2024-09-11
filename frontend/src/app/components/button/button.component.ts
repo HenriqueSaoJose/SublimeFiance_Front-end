76bf95e0a52d109e379ce1c110f8cf1f84fb3d1f
HAndradeSJ <henriqueandradesj@outlook.com>
2024-09-11 18:49:27 -0300

    styled compoent header

--- frontend/src/app/components/button/button.component.ts
+++ frontend/src/app/components/button/button.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input,Output} from '@angular/core';
+type Variants = "primary" | "secondary";
 @Component({
   selector: 'app-button',
   standalone: true,
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-
+  @Input("text") text:string = "";
+  @Input("loading") loading : boolean = false;
+  @Input("disabled") disabled : boolean = false;
+  @Input()variants : Variants = "primary";
+  @Output("Onclick") onclick: EventEmitter<any> = new EventEmitter();
 }
--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent,NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   menuOptions: Array<String> = ["Benefits","About","Contact Us"]
+  ButtonSing(){
+    console.log("OI")
+  }
 }
